0b42e6e043f199aa36b613dc74feef7c2f5e127a
Gabriel Kutsovsky <gkutsovsky@gmail.com>
2019-03-15 11:54:49 -0400

    generate sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.
-      require(:user)
+      require(:user).
       permit(:username,
              :password,
              :password_confirmation)
